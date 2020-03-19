aa1888efd7508b133e2f62f08d76a399fc090af1
Stefan Heule <heule@google.com>
2020-03-19 09:39:20 -0700

    Setup bazel dependency on p4rt.
    Change-Id: Ide957e31f54f6a4c0d49adde01c2c4ea24e47a2e

--- hello_world/hello_world.cc
+++ hello_world/hello_world.cc
@@ -1,6 +1,9 @@
 #include <iostream>
 
+#include "p4/config/v1/p4info.pb.h"
+
 int main() {
-  std::cout << "Hello to the world from Bazel!" << std::endl;
+  p4::config::v1::P4Info info;
+  std::cout << "P4Info: " << info.DebugString() << std::endl;
   return 0;
 }
