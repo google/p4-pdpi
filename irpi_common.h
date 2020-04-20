df83fa29e6440432edc1fc1c8bcbdbad3a38f086
Atman Mehta <atmanm@google.com>
2020-04-20 13:19:47 -0700

    Add IR representation
    Change-Id: I7b40d57fba2c6132ad75c858f0c199c5c55e6569

---
+++ irpi_common.h
@@ -0,0 +1,55 @@
+#ifndef IRPI_COMMON_H
+#define IRPI_COMMON_H
+// P4 intermediate representation proto definitions for use in conversion from
+// Program-Independent to either Program-Dependent or App-DB formats
+
+#include <iostream>
+#include "absl/types/variant.h"
+#include "absl/types/optional.h"
+#include "p4/v1/p4runtime.pb.h"
+
+namespace pdpi {
+struct IrTernaryMatch {
+  std::string value;
+  std::string mask;
+};
+
+struct IrMatch {
+  std::string name;
+  absl::variant<std::string, IrTernaryMatch> value;
+};
+
+struct IrActionParam {
+  std::string name;
+  std::string value;
+};
+
+struct IrAction {
+  std::string name;
+  std::vector<IrActionParam> params;
+};
+
+struct IrTableEntry {
+  std::string table_name;
+  std::vector<IrMatch> matches;
+  absl::optional<IrAction> action;
+  absl::optional<int> priority;
+  std::string controller_metatdata;
+};
+
+// Converts a PI table entry to the IR. Throws std::invalid_argument if PI is
+// not well-formed, and throws a pdpi::internal_error exception (which is a
+// std::runtime_error) on internal errors.
+// Not implemented yet
+// IrTableEntry PiToIr(const P4InfoMetadata &metadata,
+//                     const p4::v1::TableEntry& pi);
+
+// Converts an IR table entry to the PI representation. Throws
+// std::invalid_argument if PI is not well-formed, and throws a
+// pdpi::internal_error exception (which is a std::runtime_error) on internal
+// errors.
+// Not implemented yet
+// p4::v1::TableEntry IrToPi(const P4InfoMetadata &metadata,
+//                          const IrTableEntry& ir);
+}  // namespace pdpi
+#endif  // IRPI_COMMON_H
